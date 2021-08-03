336d61e7b37f8def02eee106407f076d13fa8610
Nicola Maiorana <nmaiorana@gmail.com>
2021-08-02 20:17:05 -0700

    Testing different Alphas

--- tools/.ipynb_checkpoints/utils-checkpoint.py
+++ tools/.ipynb_checkpoints/utils-checkpoint.py
@@ -6,6 +6,11 @@ from IPython.display import Image
 import numpy as np
 import pandas as pd
 
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+
+from tqdm import tqdm
+
 #####################
 # Portfolio Functions
 #####################
@@ -246,3 +251,79 @@ def get_snp500():
     dataframe = pd.DataFrame(data=df_data, columns=df_columns)
     dataframe.set_index(['Symbol'], inplace=True)
     return dataframe
+
+########################
+# Finvis Stock Sentiment
+########################
+
+# NLTK VADER for sentiment analysis
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+finwiz_url = 'https://finviz.com/quote.ashx?t='
+
+def get_finvis_stock_sentiment(tickers):
+    
+    news_tables = {}
+
+    for ticker in tqdm(tickers, desc='Tickers', unit='Finvis Postings'):
+        url = finwiz_url + ticker
+        req = Request(url=url,headers={'user-agent': 'my-app/0.0.1'}) 
+        try:
+            response = urlopen(req)    
+        except:
+            continue
+        # Read the contents of the file into 'html'
+        html = BeautifulSoup(response, features="lxml")
+        # Find 'news-table' in the Soup and load it into 'news_table'
+        news_table = html.find(id='news-table')
+        # Add the table to our dictionary
+        news_tables[ticker] = news_table
+        
+    parsed_news = []
+
+    # Iterate through the news
+    for file_name, news_table in tqdm(news_tables.items(), desc='News Tables', unit='News Table Items'):
+        # Iterate through all tr tags in 'news_table'
+        for x in news_table.findAll('tr'):
+            # read the text from each tr tag into text
+            # get text from a only
+            text = x.a.get_text() 
+            # splite text in the td tag into a list 
+            date_scrape = x.td.text.split()
+            # if the length of 'date_scrape' is 1, load 'time' as the only element
+
+            if len(date_scrape) == 1:
+                time = date_scrape[0]
+
+            # else load 'date' as the 1st element and 'time' as the second    
+            else:
+                date = date_scrape[0]
+                time = date_scrape[1]
+            # Extract the ticker from the file name, get the string up to the 1st '_'  
+            ticker = file_name.split('_')[0]
+
+            # Append ticker, date, time and headline as a list to the 'parsed_news' list
+            parsed_news.append([ticker, date, time, text])
+            
+    vader = SentimentIntensityAnalyzer()
+
+    # Set column names
+    columns = ['ticker', 'date', 'time', 'headline']
+
+    # Convert the parsed_news list into a DataFrame called 'parsed_and_scored_news'
+    parsed_and_scored_news = pd.DataFrame(parsed_news, columns=columns)
+
+    # Iterate through the headlines and get the polarity scores using vader
+    scores = parsed_and_scored_news['headline'].apply(vader.polarity_scores).tolist()
+
+    # Convert the 'scores' list of dicts into a DataFrame
+    scores_df = pd.DataFrame(scores)
+
+    # Join the DataFrames of the news and the list of dicts
+    parsed_and_scored_news = parsed_and_scored_news.join(scores_df, rsuffix='_right')
+
+    # Convert the date column from string to datetime
+    parsed_and_scored_news['date'] = pd.to_datetime(parsed_and_scored_news.date).dt.date
+
+    return parsed_and_scored_news
